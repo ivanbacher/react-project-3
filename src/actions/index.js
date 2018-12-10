1d2861130f83f041bf77c550869f857b35999dc7
ivan <ivanbacher@users.noreply.github.com>
2018-12-10 15:34:17 +0000

    save

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import { ADD_REMINDER } from '../constants';
+
+
+export const addReminder = (text) => {
+    const action = {
+        type: ADD_REMINDER,
+        text: text
+    }
+
+    console.log('action in addReminder', action);
+
+    return action;
+}
--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,26 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div> Hello </div>
+      <div className="App">
+        <div className="title">
+            Reminder Pro
+        </div>
+
+        <div className="form-inline">
+            <div className="form-group">
+                <input
+                    className="form-control"
+                    placeholder="I have to ..."
+                />
+            </div>
+            <button
+                type="button"
+                className="btn btn-success"
+            >
+                Add Reminder
+            </button>
+        </div>
+      </div>
     );
   }
 }
